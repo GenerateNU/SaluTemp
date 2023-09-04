0b1cb08222625d8cb238f36c6333a6b86d8a83b8
brianreicher <reicher.b@northeastern.edu>
2023-09-04 18:09:09 -0400

    Adding backend template

---
+++ salutemp_backend/tests/api_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	c "generate/bootcamp/src/controller"
+	"generate/bootcamp/src/model"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/huandu/go-assert"
+	"github.com/jackc/pgx"
+)
+
+func TestGetBooks(t *testing.T) {
+	db_url, exists := os.LookupEnv("DATABASE_URL")
+
+	cfg := pgx.ConnConfig{
+		User:     "user",
+		Database: "bootcamp",
+		Password: "pwd",
+		Host:     "localhost",
+		Port:     5433,
+	}
+	var err error
+	if exists {
+		cfg, err = pgx.ParseConnectionString(db_url)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+	conn, err := pgx.Connect(cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	m := &model.PgModel{
+		Conn: conn,
+	}
+	c := &c.PgController{
+		Model: m,
+	}
+	router := c.Serve()
+
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "/v1/books/1738", nil)
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	var books model.Book
+
+	if e := json.Unmarshal(w.Body.Bytes(), &books); e != nil {
+		panic(err)
+	}
+
+	test_book := model.Book{
+		BookId: 1738,
+		Title:  "Percy Jackson and the Olympians - The Lighning Thief",
+		Author: "Rick Riordan",
+	}
+	assert.Equal(t, test_book, books)
+}
