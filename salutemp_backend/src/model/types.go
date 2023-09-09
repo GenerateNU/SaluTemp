af35e823625372c4d773f81b7c338a2493e45641
Bedrockdude10 <dannyrollo4@gmail.com>
2023-09-09 14:48:39 -0400

    Changed book & student types to medication & patient

--- salutemp_backend/src/model/types.go
+++ salutemp_backend/src/model/types.go
@@ -1,12 +1,12 @@
 package model
 
-type Book struct {
-	BookId int64  `json:"id" db:"book_id"`
+type Medication struct {
+	med_id int64  `json:"id" db:"med_id"`
 	Title  string `json:"title" db:"title"`
 	Author string `json:"author" db:"author"`
 }
 
-type Student struct {
-	NUID int64  `json:"nuid" db:"nuid"`
+type Patient struct {
+	ID   int64  `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
 }
