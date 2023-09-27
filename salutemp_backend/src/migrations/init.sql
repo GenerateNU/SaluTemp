1952cbac758932da1216b8b661ed913adb5493cd
John Buergler <118939901+john-buergler@users.noreply.github.com>
2023-09-27 10:10:55 -0400

    db impl of first schema, with population for test. foreign key issue to be resolved

--- salutemp_backend/src/migrations/init.sql
+++ salutemp_backend/src/migrations/init.sql
@@ -1,33 +1,73 @@
-CREATE TABLE IF NOT EXISTS patients (
-    id integer PRIMARY KEY,
-    name varchar NOT NULL
+DROP TYPE IF EXISTS condition_type;
+DROP TABLE IF EXISTS medication_constraint;
+DROP TABLE IF EXISTS condition_event;
+DROP TABLE IF EXISTS alert;
+DROP TABLE IF EXISTS stored_medication;
+DROP TABLE IF EXISTS medication;
+DROP TABLE IF EXISTS "user";
+
+CREATE TYPE condition_type AS ENUM ('TEMPERATURE', 'HUMIDITY', 'LIGHT_EXPOSURE');
+
+CREATE TABLE IF NOT EXISTS "user" (
+    user_id integer NOT NULL UNIQUE,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    email varchar NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS medications (
-    med_id integer PRIMARY KEY,
-    title varchar NOT NULL,
-    author varchar NOT NULL
+CREATE TABLE IF NOT EXISTS medication (
+    medication_id integer NOT NULL UNIQUE,
+    medication_name varchar NOT NULL,
+    PRIMARY KEY (medication_id)
 );
 
-CREATE TABLE IF NOT EXISTS checked_out_medications (
-    checkout_id serial PRIMARY KEY,
-    med_id integer NOT NULL REFERENCES medications (med_id),
-    id integer NOT NULL REFERENCES patients (id),
-    expected_return_date timestamp with time zone NOT NULL
+CREATE TABLE IF NOT EXISTS stored_medication ( /*medication_instance?*/
+    stored_medication_id integer NOT NULL UNIQUE,
+    medication_id integer NOT NULL,
+    user_id integer NOT NULL,
+    current_temperature float,
+    current_humidity float,
+    current_light float,
+    PRIMARY KEY (stored_medication_id),
+    FOREIGN KEY (medication_id) REFERENCES medication (medication_id),
+    FOREIGN KEY (user_id) REFERENCES "user" (user_id)
+    );
+
+CREATE TABLE IF NOT EXISTS alert (
+    warning_id integer NOT NULL UNIQUE,
+    stored_medication_id integer NOT NULL,
+    user_id integer NOT NULL,
+    warning_timestamp timestamp NOT NULL,
+    warning_description varchar NOT NULL,
+    condition_type condition_type NOT NULL,
+    PRIMARY KEY (warning_id),
+    FOREIGN KEY (user_id, stored_medication_id) REFERENCES stored_medication (user_id, stored_medication_id)
 );
 
-CREATE TABLE IF NOT EXISTS holds (
-    hold_id serial PRIMARY KEY,
-    med_id integer NOT NULL REFERENCES medications (med_id),
-    id integer NOT NULL REFERENCES patients (id),
-    hold_creation_date timestamp with time zone NOT NULL
+CREATE TABLE IF NOT EXISTS condition_event (
+    event_time timestamp NOT NULL,
+    stored_medication_id integer NOT NULL,
+    temperature float,
+    humidity float,
+    light float,
+    PRIMARY KEY (event_time, stored_medication_id),
+    FOREIGN KEY (stored_medication_id) REFERENCES stored_medication (stored_medication_id)
 );
 
-CREATE TABLE IF NOT EXISTS liked_medications (
-    like_id serial PRIMARY KEY,
-    med_id integer NOT NULL REFERENCES medications (med_id),
-    id integer NOT NULL REFERENCES patients (id)
+CREATE TABLE IF NOT EXISTS medication_constraint (
+    medication_id integer NOT NULL UNIQUE,
+    condition_type condition_type NOT NULL,
+    max_threshold float,
+    min_threshold float,
+    duration varchar, /*Not sure if we should store this as a time object.*/
+    PRIMARY KEY (medication_id, condition_type),
+    FOREIGN KEY (medication_id) REFERENCES medication (medication_id)
 );
 
-
-INSERT INTO medications (med_id, title, author) VALUES (1738, 'The Lightning Thief', 'Rick Riordan');
+INSERT INTO "user" VALUES (200, 'John', 'Doe', 'johndoe@gmail.com');
+INSERT INTO medication VALUES (301, 'TestMed');
+INSERT INTO stored_medication VALUES (1, 301, 200, 70, 20, 20);
+INSERT INTO alert VALUES (3, 1, 200, current_timestamp, 'Test Description!', 'TEMPERATURE');
+INSERT INTO condition_event VALUES (current_timestamp, 1, 70, 20, 20);
+INSERT INTO medication_constraint VALUES (301, 'TEMPERATURE', 90, 50, '2 Days, 2 Hours, 10 Minutes');
