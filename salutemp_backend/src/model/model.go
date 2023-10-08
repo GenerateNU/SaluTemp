f0b138664ce24c27ebb287be55db8ebb8a378fd3
Bedrockdude10 <dannyrollo4@gmail.com>
2023-10-08 14:52:35 -0400

    Renamed functions/fixed nil returns for delete route. Need to follow the same design for all the other routes

--- salutemp_backend/src/model/model.go
+++ salutemp_backend/src/model/model.go
@@ -1,102 +1,107 @@
 package model
 
 import (
-    "github.com/jackc/pgx"
+	"github.com/jackc/pgx"
 )
 
 type PgModel struct {
-    Conn *pgx.Conn
+	Conn *pgx.Conn
 }
 
 type Model interface {
-    Medication(int64) Medication
-    AllMedications() ([]Medication, error)
-    AddMedication(Medication) (Medication, error)
-    DeleteMedication(int64) error
-    EditMedication(Medication) error
+	Medication(int64) Medication
+	AllMedications() ([]Medication, error)
+	AddMedication(Medication) (Medication, error)
+	DeleteMedication(int64) error
+	EditMedication(Medication) error
 
-    Patient(int64) Patient
-    AllPatients() ([]Patient, error)
-    AddPatient(Patient) (Patient, error)
-    DeletePatient(int64) error
-    EditPatient(Patient) error
+	Patient(int64) Patient
+	AllPatients() ([]Patient, error)
+	AddPatient(Patient) (Patient, error)
+	DeletePatient(int64) error
+	EditPatient(Patient) error
 }
 
 func (m *PgModel) Medication(id int64) Medication {
-    med, err := GetMedFromDB(m.Conn, id)
+	med, err := GetMedFromDB(m.Conn, id)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    return med
+	return med
 }
 
 func (m *PgModel) AddMedication(med Medication) (Medication, error) {
-    b, err := WriteMedToDb(m.Conn, med)
+	b, err := WriteMedToDb(m.Conn, med)
 
-    if err != nil {
-        return Medication{}, err
-    }
+	if err != nil {
+		return Medication{}, err
+	}
 
-    return b, nil
+	return b, nil
 }
 
+// Function to delete a single medication from the list of medications
 func (m *PgModel) DeleteMedication(id int64) error {
-    err := DeleteMedication(m.Conn, id)
-    return err
+	err := DeleteMedFromDB(m.Conn, id)
+	// Return error if there is an error
+	if err != nil {
+		return err
+	}
+	// Nothing to return upon a successful deletion
+	return nil
 }
 
 func (m *PgModel) EditMedication(med Medication) error {
-    err := EditMedication(m.Conn, med)
-    return err
+	err := EditMedication(m.Conn, med)
+	return err
 }
 
 func (m *PgModel) AllMedications() ([]Medication, error) {
-    meds, err := GetAllMedsFromDB(m.Conn)
+	meds, err := GetAllMedsFromDB(m.Conn)
 
-    if err != nil {
-        return []Medication{}, err
-    }
-    return meds, nil
+	if err != nil {
+		return []Medication{}, err
+	}
+	return meds, nil
 }
 
 func (m *PgModel) Patient(id int64) Patient {
-    patient, err := GetPatientFromDB(m.Conn, id)
+	patient, err := GetPatientFromDB(m.Conn, id)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    return patient
+	return patient
 }
 
 func (m *PgModel) AddPatient(patient Patient) (Patient, error) {
-    p, err := WritePatientToDb(m.Conn, patient)
+	p, err := WritePatientToDb(m.Conn, patient)
 
-    if err != nil {
-        return Patient{}, err
-    }
+	if err != nil {
+		return Patient{}, err
+	}
 
-    return p, nil
+	return p, nil
 }
 
 func (m *PgModel) DeletePatient(id int64) error {
-    err := DeletePatient(m.Conn, id)
-    return err
+	err := DeletePatient(m.Conn, id)
+	return err
 }
 
 func (m *PgModel) EditPatient(patient Patient) error {
-    err := EditPatient(m.Conn, patient)
-    return err
+	err := EditPatient(m.Conn, patient)
+	return err
 }
 
-
 func (m *PgModel) AllPatients() ([]Patient, error) {
-    pat, err := GetAllPatientsFromDB(m.Conn)
+	pat, err := GetAllPatientsFromDB(m.Conn)
 
-    if err != nil {
-        return []Patient{}, err
-    }
-    return pat, nil
+	if err != nil {
+		return []Patient{}, err
+	}
+	return pat, nil
 }
--- salutemp_backend/src/model/transactions.go
+++ salutemp_backend/src/model/transactions.go
@@ -7,22 +7,28 @@ import (
 )
 
 func WriteMedToDb(pool *pgx.Conn, med Medication) (Medication, error) {
-    var insertedMed Medication
-    err := pool.QueryRow("INSERT INTO medications (med_id,title, author) VALUES ($1, $2, $3) RETURNING med_id;", med.Title, med.Author,med.MedID).Scan(&insertedMed.MedID)
+	var insertedMed Medication
+	err := pool.QueryRow("INSERT INTO medications (med_id,title, author) VALUES ($1, $2, $3) RETURNING med_id;", med.Title, med.Author, med.MedID).Scan(&insertedMed.MedID)
 
-    if err != nil {
-        return Medication{}, err
-    }
+	if err != nil {
+		return Medication{}, err
+	}
 
-    return insertedMed, nil
+	return insertedMed, nil
 }
 
+// Function to delete a given medication from the medication table
+func DeleteMedFromDB(pool *pgx.Conn, medID int64) error {
+	_, err := pool.Exec(fmt.Sprintf("DELETE FROM medications WHERE med_id = %d;", medID))
 
-func DeleteMedication(pool *pgx.Conn, medID int64) error {
-    _, err := pool.Exec(fmt.Sprintf("DELETE FROM medications WHERE med_id = %d;", medID))
-    return err
+	if err != nil {
+		return err
+	} else {
+		// return nil because there is nothing to return upon a successful deltion
+		return nil
+	}
+
 }
-
 
 func GetMedFromDB(pool *pgx.Conn, med_id int64) (Medication, error) {
 	med := Medication{
@@ -64,16 +70,13 @@ func GetAllMedsFromDB(pool *pgx.Conn) ([]Medication, error) {
 	return results, nil
 }
 
-
-
 func EditMedication(pool *pgx.Conn, med Medication) error {
-    _, err := pool.Exec(
-        "UPDATE medications SET title = $2, author = $3 WHERE med_id = $1",
-        med.MedID, med.Title, med.Author,
-    )
-    return err
+	_, err := pool.Exec(
+		"UPDATE medications SET title = $2, author = $3 WHERE med_id = $1",
+		med.MedID, med.Title, med.Author,
+	)
+	return err
 }
-
 
 ///patients transactions
 
@@ -81,73 +84,71 @@ func EditMedication(pool *pgx.Conn, med Medication) error {
 
 // WritePatientToDb inserts a new patient record into the database and returns the updated patient object with the assigned ID.
 func WritePatientToDb(pool *pgx.Conn, patient Patient) (Patient, error) {
-    err := pool.QueryRow(
-        "INSERT INTO patients (name) VALUES ($1) RETURNING id;",
-        patient.Name,
-    ).Scan(&patient.ID)
+	err := pool.QueryRow(
+		"INSERT INTO patients (name) VALUES ($1) RETURNING id;",
+		patient.Name,
+	).Scan(&patient.ID)
 
-    if err != nil {
-        return Patient{}, err
-    }
+	if err != nil {
+		return Patient{}, err
+	}
 
-    return patient, nil
+	return patient, nil
 }
 
 // GetPatientFromDB retrieves a patient record from the database based on the given patient ID.
 func GetPatientFromDB(pool *pgx.Conn, id int64) (Patient, error) {
-    patient := Patient{
-        ID: id,
-    }
+	patient := Patient{
+		ID: id,
+	}
 
-    err := pool.QueryRow(
-        "SELECT * FROM patients WHERE id = $1;",
-        id,
-    ).Scan(&patient.ID, &patient.Name)
+	err := pool.QueryRow(
+		"SELECT * FROM patients WHERE id = $1;",
+		id,
+	).Scan(&patient.ID, &patient.Name)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    return patient, nil
+	return patient, nil
 }
 
 // GetAllPatientsFromDB retrieves all patient records from the database.
 func GetAllPatientsFromDB(pool *pgx.Conn) ([]Patient, error) {
-    rows, err := pool.Query("SELECT * patients;")
+	rows, err := pool.Query("SELECT * patients;")
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    results := []Patient{}
+	results := []Patient{}
 
-    defer rows.Close()
+	defer rows.Close()
 
-    for rows.Next() {
-        patient := Patient{}
-        err := rows.Scan(&patient.ID, &patient.Name)
+	for rows.Next() {
+		patient := Patient{}
+		err := rows.Scan(&patient.ID, &patient.Name)
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        results = append(results, patient)
-    }
+		results = append(results, patient)
+	}
 
-    return results, nil
+	return results, nil
 }
 
-
 func DeletePatient(pool *pgx.Conn, id int64) error {
-    _, err := pool.Exec(fmt.Sprintf("DELETE FROM patients WHERE id = %d;", id))
-    return err
+	_, err := pool.Exec(fmt.Sprintf("DELETE FROM patients WHERE id = %d;", id))
+	return err
 }
 
 func EditPatient(pool *pgx.Conn, user Patient) error {
-    _, err := pool.Exec(
-        "UPDATE patients SET name = $2 WHERE id = $1",
-        user.ID, user.Name,
-    )
-    return err
+	_, err := pool.Exec(
+		"UPDATE patients SET name = $2 WHERE id = $1",
+		user.ID, user.Name,
+	)
+	return err
 }
-
