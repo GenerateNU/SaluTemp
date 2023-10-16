620aaa3ee79b6b749d56e67ab41c67f43e289aa6
Briana Torres <briana@torrez.us>
2023-10-16 17:06:53 -0400

    added struct for checked_out_meds table

--- salutemp_backend/src/model/types.go
+++ salutemp_backend/src/model/types.go
@@ -10,3 +10,16 @@ type Patient struct {
 	ID   int64  `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
 }
+
+type CheckedOutMedication struct {
+    CheckoutID         int64     `json:"checkout_id" db:"checkout_id"`
+    MedID              int64     `json:"med_id" db:"med_id"`
+    ID                 int64     `json:"id" db:"id"`
+    ExpectedReturnDate time.Time `json:"expected_return_date" db:"expected_return_date"`
+}
+
+
+
+
+
+
