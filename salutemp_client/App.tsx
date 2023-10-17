80a5898ee5a0a2fdda6219fa51831f0c95f04b6b
Bella <63426032+hungrybellie@users.noreply.github.com>
2023-10-17 06:15:10 -0700

    stack navigation attempted

--- salutemp_client/App.tsx
+++ salutemp_client/App.tsx
@@ -1,16 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import MedOverviewScreen from './screens/MedOverviewScreen';
 import NewMedScreen from './screens/NewMedScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MedicationsList from './screens/MedicationsList';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <MedOverviewScreen />
-    // <NewMedScreen />
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Medication Overview"
+          component={MedOverviewScreen}
+        />
+        <Stack.Screen
+          name="New"
+          component={NewMedScreen}
+        />
+        <Stack.Screen
+        name="MedList"
+        component={MedicationsList}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
