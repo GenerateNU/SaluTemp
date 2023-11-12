266ed0fc521bf59b788922ded12cac8c4c07fd46
Bella <63426032+hungrybellie@users.noreply.github.com>
2023-11-12 14:41:33 -0800

    stateful question mark 😧

--- salutemp_client/screens/NewMedScreen.tsx
+++ salutemp_client/screens/NewMedScreen.tsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { GetEntryInfo } from '../api/MedicationInfoApi';
 import colors from '../config/colors';
-import { EntryInfo } from '../types';
 
 interface NewMedScreenProps {
   // Define any props if necessary
@@ -13,6 +11,16 @@ interface NewMedScreenProps {
 const NewMedScreen = () => {
 
   const navigation = useNavigation();  
+  const [medName, setMedName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [date, setDate] = useState('');
+  const [maxTemp, setMaxTemp] = useState('');
+  const [minTemp, setMinTemp] = useState('');
+  const [maxHumid, setMaxHumid] = useState('');
+  const [minHumid, setMinHumid] = useState('');
+  const [maxLight, setMaxLight] = useState('');
+  const [minLight, setMinLight] = useState('');
+
   
   return (
     <SafeAreaView style={styles.container}>
@@ -30,17 +38,25 @@ const NewMedScreen = () => {
         <Text style={styles.title}>Medication Info</Text>
         <View style={styles.columnStyle}>
           <Text>Medication Name</Text>
-          <TextInput style={styles.textInputThinLong}></TextInput>
+          <TextInput style={styles.textInputThinLong}
+          onChangeText={text => setMedName(text)}
+          value={medName}
+          ></TextInput>
         </View>
         <View style={styles.columns}>
           <View style={styles.columnStyle}>
             <Text>Nickname (Optional)</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setNickname(text)}
+            value={nickname}></TextInput>
           </View>
           <View style={styles.columnStyle}>
             <Text>Expiration Date</Text>
+            // not sure if date should be left as a string, but this can be changed 
             <TextInput style={styles.textInputThin}
-              placeholder="mm / dd / yy"></TextInput>
+              placeholder="mm / dd / yy"
+              onChangeText={text => setDate(text)}
+              value={date}></TextInput>
           </View>
         </View>
       </View>
@@ -55,20 +71,32 @@ const NewMedScreen = () => {
         <View style={styles.columns}>
           <View style={styles.columnStyle}>
             <Text>Max Temp</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMaxTemp(text)}
+            value={maxTemp}></TextInput>
             <Text>Max Humidity</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMaxHumid(text)}
+            value={maxHumid}></TextInput>
             <Text>Max Light</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMaxLight(text)}
+            value={maxLight}></TextInput>
           </View>
 
           <View style={styles.columnStyle}>
             <Text>Min Temp</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMinTemp(text)}
+            value={minTemp}></TextInput>
             <Text>Min Humidity</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMinHumid(text)}
+            value={minHumid}></TextInput>
             <Text>Min Light</Text>
-            <TextInput style={styles.textInputThin}></TextInput>
+            <TextInput style={styles.textInputThin}
+            onChangeText={text => setMinLight(text)}
+            value={minLight}></TextInput>
           </View>
         </View>
       </View>
