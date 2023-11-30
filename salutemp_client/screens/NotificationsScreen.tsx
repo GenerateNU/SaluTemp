e21f1a91d4dda488732b58e64dd84fe56497035d
Briana Torres <briana@torrez.us>
2023-11-30 18:18:05 -0500

    updated notif screen

--- salutemp_client/screens/NotificationsScreen.tsx
+++ salutemp_client/screens/NotificationsScreen.tsx
@@ -1,72 +1,88 @@
 import React from 'react';
-import { StyleSheet, View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-
 import colors from '../config/colors';
 
-function NotificationsScreen() {
-    // Example notification data
-    const notifications = [
-        { id: 1, message: 'X medication has raised 3 degrees, consider moving', time: '12 minutes ago' },
-        { id: 2, message: 'X medication has raised 3 degrees, consider moving', time: '12 minutes ago' },
-        // add more notifs here 
-    ];
+type Notification = {
+  id: string;
+  title: string;
+  date: string;
+};
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerTitle}>Notifications</Text>
-                <TouchableOpacity onPress={() => {}}>
-                    <MaterialIcons name="close" size={28} color={colors.darkNeutral} />
-                </TouchableOpacity>
-            </View>
-            <ScrollView style={styles.notificationsList}>
-                {notifications.map((notification) => (
-                    <View key={notification.id} style={styles.notificationCard}>
-                        <Text style={styles.notificationMessage}>{notification.message}</Text>
-                        <Text style={styles.notificationTime}>{notification.time}</Text>
-                    </View>
-                ))}
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+const notifications: Notification[] = [
+  { id: '1', title: 'X medication has raised 3 degrees, consider moving', date: 'Today' },
+  // Add more notifications here
+];
+
+const NotificationScreenComponent: React.FC = () => {
+
+  const renderItem = ({ item }: { item: Notification }) => (
+    <View style={styles.notificationItem}>
+      <View style={styles.notificationContent}>
+        <Text style={styles.notificationTitle}>{item.title}</Text>
+        <Text style={styles.notificationDate}>{item.date}</Text>
+      </View>
+      <TouchableOpacity onPress={() => {/* handle press */}}>
+        <MaterialIcons name="chevron-right" size={25} color={colors.darkNeutral} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Notifications</Text>
+      </View>
+      <FlatList
+        data={notifications}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContainer}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.background,
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 20,
-    },
-    headerTitle: {
-        fontSize: 20,
-        fontWeight: '500',
-        color: colors.darkNeutral,
-    },
-    notificationsList: {
-        padding: 20,
-    },
-    notificationCard: {
-        backgroundColor: colors.lightNeutral,
-        borderRadius: 10,
-        padding: 20,
-        marginBottom: 10,
-    },
-    notificationMessage: {
-        fontSize: 16,
-        color: colors.darkNeutral,
-    },
-    notificationTime: {
-        fontSize: 12,
-        color: colors.lightNeutral,
-        marginTop: 4,
-    },
-    // ... any additional styles you may need
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    padding: 16,
+    backgroundColor: colors.darkNeutral,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.white,
+  },
+  listContainer: {
+    padding: 8,
+  },
+  notificationItem: {
+    backgroundColor: colors.lightNeutral,
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grey,
+  },
+  notificationContent: {
+    flex: 1,
+  },
+  notificationTitle: {
+    fontSize: 16,
+    color: colors.bodyText,
+  },
+  notificationDate: {
+    fontSize: 12,
+    color: colors.grey,
+  },
 });
 
-export default NotificationsScreen;
+export default NotificationScreenComponent;
+
