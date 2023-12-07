fbe5408f71ec60cc4b1177e6cb57c6fbf956a7e9
muneerlalji <muneerlalji@gmail.com>
2023-12-07 15:21:34 -0500

    AUTH PERSISTENCE WOOOO

--- salutemp_client/firebaseConfig.ts
+++ salutemp_client/firebaseConfig.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import AsyncStorage from "@react-native-async-storage/async-storage"
-
-// Optionally import the services that you want to use
-import { initializeAuth, indexedDBLocalPersistence } from "firebase/auth";
+import { initializeAuth, setPersistence, getReactNativePersistence  } from "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -14,7 +12,8 @@ const firebaseConfig = {
     appId: "1:337676177477:web:d25fb271f569160966932c",
     measurementId: "G-ZZGBDHJ1RW"
   };
+
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {persistence: indexedDBLocalPersistence});
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {persistence: getReactNativePersistence(AsyncStorage)});
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
